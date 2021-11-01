52802f1e6ce8f9c5d82d20c90b14882b059bd9d9
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-11-01 17:02:14 +0000

    bookmarks page feature test

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -4,3 +4,11 @@ feature 'Viewing bookmarks' do
     expect(page).to have_content 'Bookmark manager'
   end
 end
+
+feature 'Viewing bookmarks' do
+  scenario 'visiting bookmarks page' do
+    visit('/bookmarks')
+    expect(page).to have_content 'https://www.bstacademy.co.uk/'
+    expect(page).to have_content 'https://www.utcuk.com/'
+  end
+end
